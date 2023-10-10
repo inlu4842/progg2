dd4866cd4904a05f4a49036a3a598380aec4eee7
inlu4842 <ines.lundstrom.4842@student.uu.se>
2023-10-10 13:36:40 +0200

    implementing fib

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-
+from numba import njit
+import time
+import matplotlib.pyplot as plt
+import numpy as np
 from person import Person
 
 def main():
@@ -10,3 +13,18 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
+def fib_py(n):
+# Python Fibonacci without Numba
+    if n <= 1:
+        return n
+    else:
+        return fib_py(n - 1) + fib_py(n - 2)
+    
+# Numba-optimized Fibonacci
+#@jit
+def fib_numba(n):
+    if n <= 1:
+        return n
+    else:
+        return fib_numba(n - 1) + fib_numba(n - 2)
