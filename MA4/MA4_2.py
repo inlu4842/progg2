4e92dae715f0e7e4c7885c038a93a17c98722642
inlu4842 <ines.lundstrom.4842@student.uu.se>
2023-10-10 17:33:18 +0200

    change in fib location in MA4_2

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -3,6 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from person import Person
 
+def fib_py(n):
+# Python Fibonacci without Numba
+    if n <= 1:
+        return n
+    else:
+        return fib_py(n - 1) + fib_py(n - 2)
+    
+# Numba-optimized Fibonacci
+@njit
+def fib_numba(n):
+    if n <= 1:
+        return n
+    else:
+        return fib_numba(n - 1) + fib_numba(n - 2)
+    
+
 def main():
      f = Person(5)
      print(f.get())
@@ -21,20 +37,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
-def fib_py(n):
-# Python Fibonacci without Numba
-    if n <= 1:
-        return n
-    else:
-        return fib_py(n - 1) + fib_py(n - 2)
-    
-# Numba-optimized Fibonacci
-@njit
-def fib_numba(n):
-    if n <= 1:
-        return n
-    else:
-        return fib_numba(n - 1) + fib_numba(n - 2)
-    
     
