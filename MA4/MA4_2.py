b7afaa2f6a57c554d75d171ceb78a5b693a1e2aa
inlu4842 <ines.lundstrom.4842@student.uu.se>
2023-10-11 08:39:17 +0200

    another edit in f.fib()

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -22,8 +22,8 @@ def fib_numba(n):
 def main():
      f = Person(5)
      print(f.get())
-     #f.set(8)
-     #print(f.get())
+     f.set(8)
+     print(f.get())
 
      k= fib_py(5)
      print(k)
