8bfaea6ff5584c5fcf78a1a47f1e3316ef5cf572
inlu4842 <ines.lundstrom.4842@student.uu.se>
2023-10-11 10:01:10 +0200

    implementing time

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -2,6 +2,7 @@ from numba import njit
 import matplotlib.pyplot as plt
 import numpy as np
 from person import Person
+import time
 
 def fib_py(n):
 # Python Fibonacci without Numba
@@ -17,6 +18,39 @@ def fib_numba(n):
         return n
     else:
         return fib_numba(n - 1) + fib_numba(n - 2)
+    
+def time_fib_py(n):
+    start_time = time.perf_counter()
+    fib_py(n)
+    end_time = time.perf_counter()
+    return end_time - start_time
+
+def time_fib_numba(n):
+    start_time = time.perf_counter()
+    fib_numba(n)
+    end_time = time.perf_counter()
+    return end_time - start_time
+
+def time_fib_cpp(n):
+    f = Person(n)
+    start_time = time.perf_counter()
+    f.fib()
+    end_time = time.perf_counter()
+    return end_time - start_time
+
+n_values = list(range(30, 46))
+timings_py = [time_fib_py(n) for n in n_values]
+timings_numba = [time_fib_numba(n) for n in n_values]
+timings_cpp = [time_fib_cpp(n) for n in n_values]
+
+plt.figure(figsize=(10, 6))
+plt.plot(n_values, timings_py, label='Python Fibonacci')
+plt.plot(n_values, timings_numba, label='Numba Fibonacci')
+plt.plot(n_values, timings_cpp, label='C++ Fibonacci')
+plt.xlabel('n')
+plt.ylabel('Time (seconds)')
+plt.legend()
+plt.savefig('fib_timings.png')
     
 
 def main():
