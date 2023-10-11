9a06b913ef874dd223b533cd7e4cd1b4656b0059
inlu4842 <ines.lundstrom.4842@student.uu.se>
2023-10-11 13:22:27 +0200

    major changes in person cpp and py

--- MA4/person.cpp
+++ MA4/person.cpp
@@ -6,10 +6,10 @@ class Person{
 		Person(int);
 		int get();
 		void set(int);
-		int fib(int);
+		long fib(int);
 	private:
 		int age;
-		int calculateFibonacci(int);  // Private Fibonacci calculation method
+		long calculateFibonacci(int);  // Private Fibonacci calculation method
 	};
  
 Person::Person(int n){
@@ -24,22 +24,22 @@ void Person::set(int n){
 	age = n;
 	}
 
-int Person::calculateFibonacci(int n){
+long Person::calculateFibonacci(int n){
 	if (n <= 1)
 	 	return n;
     else
     	return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
 }
 
-int Person::fib(int n) {
-    return calculateFibonacci(n);
+long Person::fib() {
+    return calculateFibonacci();
 }
 
 extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
 	void Person_set(Person* person, int n) {person->set(n);}
-	int Person_fib(Person * person, int n) {return person->fib(n);}
+	long Person_fib(Person * person, int n) {return person->fib();}
 	void Person_delete(Person* person){
 		if (person){
 			delete person;
