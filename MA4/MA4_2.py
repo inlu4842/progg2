c9d4e62a2da44b8dc4dda2079788999ca20444a0
inlu4842 <ines.lundstrom.4842@student.uu.se>
2023-10-11 14:51:22 +0200

    timingss

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -22,22 +22,52 @@ def fib_numba(n):
 
 
 def main():
-     f = Person(5)
-     print(f.get())
-     f.set(8)
-     print(f.get())
+     
+     n_values = list(range(30, 46))
+     timings_py = []
+     timings_numba = []
+     timings_cpp = []
 
-     python_fib8 = fib_py(8)
-     print(f'Fib(8) using Python: {python_fib8}')
+     for n in n_values:
+          start_time = time.perf_counter()
+          fib_py(n)
+          end_time = time.perf_counter()
+          timings_py.append(end_time - start_time)
+          
+          start_time = time.perf_counter()
+          fib_numba(n)
+          end_time = time.perf_counter()
+          timings_numba.append(end_time - start_time)          
+          
+          f = Person(n)
+          start_time = time.perf_counter()
+          f.fibc()
+          end_time = time.perf_counter()
+          timings_cpp.append(end_time - start_time)
+          
+          
+          plt.figure(figsize=(10, 6))
+          plt.plot(n_values, timings_py, label='Python Fibonacci')
+          plt.plot(n_values, timings_numba, label='Numba Fibonacci')
+          plt.plot(n_values, timings_cpp, label='C++ Fibonacci')
+          plt.xlabel('n')
+          plt.ylabel('Time (seconds)')
+          plt.legend()
+          plt.savefig('fib_timings.png')
 
-     numba_fib8 = fib_numba(8)
-     print(f'Fib(8) using Numba: {numba_fib8}')
+
+     #f = Person(5)
+     #print(f.get())
+     #f.set(8)
+     #print(f.get()) #Now f = Person(8)
+
+     #python_fib8 = fib_py(8)
+     #print(f'Fib(8) using Python: {python_fib8}')
+     #numba_fib8 = fib_numba(8)
+     #print(f'Fib(8) using Numba: {numba_fib8}')
+     #print(f'Fib(8) using C++: {f.fibc()}')
 
 
-
-     #print(f'Fib(8) using C++: {f.fib()}')
-     #f = Person(6)
-     print(f.fibc())
 
 
 if __name__ == '__main__':
