5cb1f8d1c9da4f6a3c5bfeb60ae79d658eb520c2
inlu4842 <ines.lundstrom.4842@student.uu.se>
2023-10-11 14:58:36 +0200

    removing py

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -24,15 +24,15 @@ def fib_numba(n):
 def main():
      
      n_values = list(range(30, 46))
-     timings_py = []
+     #timings_py = []
      timings_numba = []
      timings_cpp = []
      
      for n in n_values:
-          start_time = time.perf_counter()
-          fib_py(n)
-          end_time = time.perf_counter()
-          timings_py.append(end_time - start_time)
+          #start_time = time.perf_counter()
+          #fib_py(n)
+          #end_time = time.perf_counter()
+          #timings_py.append(end_time - start_time)
           
           start_time = time.perf_counter()
           fib_numba(n)
@@ -46,7 +46,7 @@ def main():
           timings_cpp.append(end_time - start_time)
 
      plt.figure(figsize=(10, 6))
-     plt.plot(n_values, timings_py, label='Python Fibonacci')
+     #plt.plot(n_values, timings_py, label='Python Fibonacci')
      plt.plot(n_values, timings_numba, label='Numba Fibonacci')
      plt.plot(n_values, timings_cpp, label='C++ Fibonacci')
      plt.xlabel('n')
